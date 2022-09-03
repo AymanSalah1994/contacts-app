8db39ee5b44d70736005faee067597dedadca8a3
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-03 18:18:11 +0200

    Added Routes and Controller for Profile

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Contact as Contact;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Settings\AccountController;
-
+use App\Http\Controllers\Settings\ProfileController ; 
 Route::get('/', function () {
     return view('welcome');
 });
@@ -24,11 +24,13 @@ Route::resources([
     '/companies' => CompanyController::class
 ]);
 
-
-
-
 Auth::routes(['verify' => true]);
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+
+
 // Route::middleware('auth')->group(function () {}) ; 
 Route::get('/settings/account', [AccountController::class, 'index']);
+Route::get('/settings/profile', [ProfileController::class, 'edit'])->name('settings.profile.edit');
+Route::put('/settings/profile', [ProfileController::class, 'update'])->name('settings.profile.update');
