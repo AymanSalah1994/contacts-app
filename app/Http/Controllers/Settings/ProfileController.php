00f4720a49b3fe0f04fb558e795eefcf4d1b5b07
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-03 18:42:01 +0200

    Added Profile Views

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -7,5 +7,20 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth' , 'verified']) ; 
+        // This Means the Profile page Only Accesses if the user is Auth , 
+        // And When he is "auth" we will show his Data ! using request()->user()
+    }
     //
+    public function edit () {
+        // return "This si Edit Get Page" ; 
+        return view('settings.profile' , [
+            'user' => auth()->user()
+        ]) ; 
+    }
+    public function update() {
+
+    }
 }
